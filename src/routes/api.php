f1933dd76624f5a60189ed51c3411a3666475f3f
Jules Renck <julesrenck@gmail.com>
2025-08-02 10:04:01 -0300

    feat: registra rotas em grupo para usuários

---
+++ src/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
